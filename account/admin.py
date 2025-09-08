64bd84fe83cd31556e39de8685f39a08a686eae5
Mac <mac@Taglines-iMac-Pro-106.local>
2025-09-08 15:08:41 +0530

    register user models in admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser  
+    list_display = ('email', 'role', 'is_staff', 'is_active')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Role Info', {'fields': ('role',)}), 
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role', 'is_staff', 'is_active'),
+        }),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
