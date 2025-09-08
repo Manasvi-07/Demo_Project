cde06fcd40ff761a97829ace6f12edec4d37b516
Mac <mac@Taglines-iMac-Pro-106.local>
2025-09-08 15:24:44 +0530

    implement serializers and views

---
+++ task/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Project, Task
+from account.models import CustomUser
+from account.enums import RoleChoices
+
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.id")
+
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description', 'deadline', 'owner', 'created_at', 'updated_at']
+
+class TaskSerializer(serializers.ModelSerializer):
+    created = serializers.ReadOnlyField(source="created.id")
+
+    class Meta:
+        model = Task
+        fields = ['id', 'project','title', 'description', 'status', 'priority', 'assigned', 'created','due_date', 'created_at', 'updated_at']
+
+    def validate_assigned(self, value):
+        if value is None:
+            return value
+        request = self.context['request']
+        user = request.user
+
+        if value.role == RoleChoices.ADMIN:
+            raise serializers.ValidationError("Tasks cannot assigned to admin user")
+
+        if user.role == RoleChoices.MANAGER:
+            if value == user:
+                raise serializers.ValidationError("Tasks cannot be assigned to manager users.")
+            if value.role != RoleChoices.DEVELOPER:
+                raise serializers.ValidationError("Managers can only assign tasks to developer.")
+            
+        if user.role == RoleChoices.DEVELOPER:
+            raise serializers.ValidationError("developer are not allowed to assign tasks.")
+        return value
---
+++ task/views.py
@@ -0,0 +1,70 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import Project, Task
+from task.serializers import ProjectSerializer, TaskSerializer
+from task.permissions import IsAdminOrManager, IsAdminManagerOrTaskOwner
+from account.enums import RoleChoices
+import logging
+from task.tasks import send_mail_task_notification
+from task.utils import notify_task_update
+
+logger = logging.getLogger("task")
+
+class ProjectView(generics.ListCreateAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == RoleChoices.ADMIN:
+            return Project.objects.all()
+        return Project.objects.filter(owner=user)
+    
+    def perform_create(self, serializer):
+        if self.request.user.role == RoleChoices.DEVELOPER:
+            raise PermissionDenied("Developer can not create project")
+        serializer.save(owner=self.request.user)
+
+class ProjectDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Project.objects.all()
+
+class TaskView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrManager]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["project", "due_date", "status", "priority"]
+    ordering_fields = ["due_date", "priority", "created_at"]
+    search_fields = ['title', "description", "assigned__email"]
+
+    def perform_create(self, serializer):
+        task = serializer.save(created=self.request.user)
+        logger.info(
+            f"Task created: {task.title} "
+            f"(Project: {task.project.title if task.project else 'No Project'}, "
+            f"Assigned: {task.assigned.email if task.assigned else 'Unassigned'}) "
+            f"created {self.request.user.email}"
+        )
+        send_mail_task_notification.delay(task.id)
+        notify_task_update(task)
+        
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Task.objects.select_related("assigned","created", "project").order_by("-created_at")
+
+        if user.role == RoleChoices.ADMIN:
+            logger.info(f"Admin {user.email} view all task")
+            return queryset
+        elif user.role == RoleChoices.MANAGER:
+            logger.info(f"Manager {user.email} show only owned task created")
+            return queryset.filter(project__owner=user)
+        logger.info(f"Developer {user.email} show only owned assigned task")
+        return queryset.filter(assigned=user)           
+    
+class TaskDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated,IsAdminManagerOrTaskOwner]
+    queryset = Task.objects.all()
